a0a1dba17f2b4cbcd99f205619ea88ee4b29f9a7
amitabh <amitabhis2903@gmail.com>
2021-04-04 12:12:53 +0530

    flying_bird created in Swift UI

---
+++ Flying_Bird/Flying_Bird/Flying_BirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flying_BirdApp.swift
+//  Flying_Bird
+//
+//  Created by MacBook05 on 04/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Flying_BirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
